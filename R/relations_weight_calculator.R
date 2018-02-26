70871a7196cdbbd0db3bfb002d1b8d377cda7ec4
nevrome <clemens@nevrome.de>
2018-02-26 14:13:35 +0100

    added settings forwarding for weight calculator

--- R/relations_weight_calculator.R
+++ R/relations_weight_calculator.R
@@ -9,24 +9,24 @@
 calculate_relations_weight <- function(x, settings) {
   x %>%
     dplyr::mutate(
-      weight = unlist(purrrlyr::by_row(x, calculate_weight)[[".out"]])
+      weight = unlist(purrrlyr::by_row(x, calculate_weight, settings)[[".out"]])
     )
 }
 
 #### helper functions ####
 
-calculate_weight <- function(x) {
+calculate_weight <- function(x, settings) {
   dplyr::case_when(
-    x$type == "child_of" ~ weight_child_of(x) %>% return,
-    x$type == "sexing"   ~ weight_sexing(x) %>% return
+    x$type == "child_of" ~ weight_child_of(x, settings) %>% return,
+    x$type == "sexing"   ~ weight_sexing(x, settings) %>% return
   )
 }
 
-weight_child_of <- function(x) {
+weight_child_of <- function(x, settings) {
   1
 }
 
-weight_sexing <- function(x) {
+weight_sexing <- function(x, settings) {
   5
 }
 
