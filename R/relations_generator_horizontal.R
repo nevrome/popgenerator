d8b251e2893ddc22e49d92e707ff9c689b3d9ce7
nevrome <clemens@nevrome.de>
2018-03-25 17:26:45 +0200

    finding all people alive when one person is also alive: lifetime overlaps

--- R/relations_generator_horizontal.R
+++ R/relations_generator_horizontal.R
@@ -7,33 +7,36 @@
 #' @export
 generate_horizontal_relations <- function(settings) {
 
-  # population <- settings@population
-  # 
-  # from <- c()
-  # to <- c()
-  # type <- c()
-  # start_time <- c()
-  # end_time <- c()
-  # 
-  # pb <- utils::txtProgressBar(style = 3)
-  # for (child in 1:nrow(population)) {
-  #   
-  #   potential_parents <- get_all_humans_in_child_bearing_age_at_childbirth(settings, child)
-  #   monogamous <- is_monogamous(settings)
-  #   
-  #   
-  #   
-  #   utils::setTxtProgressBar(pb, child/nrow(population))
-  # }
-  # close(pb)
-  # 
-  # horizontal_relations <- tibble::tibble(from, to, type, start_time, end_time)
-  # 
-  # return(horizontal_relations)
+  population <- settings@population
+
+  from <- c()
+  to <- c()
+  type <- c()
+  start_time <- c()
+  end_time <- c()
+
+  pb <- utils::txtProgressBar(style = 3)
+  for (person in 1:nrow(population)) {
+
+    potential_friends <- get_all_humans_alive_in_livetime_of_human(settings, person)
+
+
+    utils::setTxtProgressBar(pb, child/nrow(population))
+  }
+  close(pb)
+
+  horizontal_relations <- tibble::tibble(from, to, type, start_time, end_time)
+
+  return(horizontal_relations)
 
 }
 
 #### helper functions ####
+
+get_all_humans_alive_in_livetime_of_human <- function(settings, id) {
+  timeframe <- seq(settings@population$birth_time[id], settings@population$death_time[id], 1)
+  get_all_humans_alive_in_timeframe(settings, timeframe)
+}
 
 get_all_humans_alive_in_timeframe <- function(settings, timeframe) {
   purrr::map(timeframe, function(x){
