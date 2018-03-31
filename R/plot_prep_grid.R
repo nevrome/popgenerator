e1b6977d63e9e0ea04a90ce66f7943ecfa6cbf0b
nevrome <clemens@nevrome.de>
2018-03-31 21:19:30 +0200

    dependencies for plotting function

--- R/plot_prep_grid.R
+++ R/plot_prep_grid.R
@@ -14,12 +14,12 @@ plot_prep_grid <- function(x, method) {
       "population_size_functions", 
       "unit_amount_functions"
     )) {
-    cols <- rainbow(length(list_of_interest), 1)
-    p <- ggplot() + 
-      xlim(c(min(timeframe[[1]]), max(timeframe[[1]])))
+    cols <- grDevices::rainbow(length(list_of_interest), 1)
+    p <- ggplot2::ggplot() + 
+      ggplot2::xlim(c(min(timeframe[[1]]), max(timeframe[[1]])))
     for (i in 1:length(list_of_interest)) {
-        p <- p + stat_function(
-          aes(y = 0),
+        p <- p + ggplot2::stat_function(
+          ggplot2::aes(y = 0),
           fun = list_of_interest[[i]], 
           colour = cols[i]
         )
