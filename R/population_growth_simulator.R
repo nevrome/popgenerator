eefbb3653000add1e83b51709ddcce6d540f54f2
nevrome <clemens@nevrome.de>
2018-04-03 15:14:27 +0200

    better documentation of simulate_growth()

--- R/population_growth_simulator.R
+++ R/population_growth_simulator.R
@@ -3,53 +3,65 @@
 #' Simulate growth of a population.
 #'
 #' @param humans tibble input population
-#' @param settings test
+#' @param settings population_settings object
 #'
-#' @return tibble output population
+#' @return data.frame with population (one row for each individual)
 #'
 #' @export
 simulate_growth <- function(humans, settings) {
 
+  # get number of last unit (to allow incrementing this number) 
   unit_counter <- humans %>% get_last_established_unit()
 
   pb <- utils::txtProgressBar(style = 3)
+  # loop through all timesteps except the initial one
   for (t in settings@time[-1]) {
     utils::setTxtProgressBar(pb, t/length(settings@time))
 
+    # let all humans age and die accordingly
     humans %<>% age()
     humans %<>% find_and_realize_deaths()
-    necessary_births <- humans %>% calculate_amount_of_necessary_births(t, settings)
+    
+    # calculate necessary amount of births to adjust the population 
+    # size to the defined population development
+    necessary_births <- humans %>% 
+      calculate_amount_of_necessary_births(t, settings)
 
-    # stop if population is big enough
+    # stop if population is big enough and no new humans have to 
+    # be generated
     if (necessary_births <= 0) next
 
-    # units handling
+    # determine amount of units and how many units should exist
     units <- humans %>% get_currently_alive_units()
     units_target_amount <- settings@unit_amount_function(t)
+    
+    # if too many units exist: get rid of some random units
     if (length(units) > units_target_amount) {
       new_unit_vector <- shrink_unit_vector(units, units_target_amount)
       humans %<>% realize_unit_deaths(new_unit_vector)
+    # if not enough units exist: create new units  
     } else if (length(units) < units_target_amount) {
       difference <- determine_amount_missing_units(units, units_target_amount)
       new_unit_vector <- enlarge_unit_vector(units, unit_counter, difference)
       unit_counter <- unit_counter + difference
+    # if amount of units correct: do nothing
     } else {
       new_unit_vector <- units
     }
 
-    # generate and add new humans
+    # generate new humans
+    new_humans <- generate_humans(
+      t = t,
+      n = necessary_births,
+      start_id = max(humans$id + 1),
+      start_age = 0,
+      settings,
+      unit_vector = new_unit_vector
+    )
+    
+    # add new humans to general humans list
     humans <- data.table::rbindlist(
-        l = list(
-          humans, 
-          generate_humans(
-            t = t,
-            n = necessary_births,
-            start_id = max(humans$id + 1),
-            start_age = 0,
-            settings,
-            unit_vector = new_unit_vector
-          )
-        ),
+        l = list(humans, new_humans),
         use.names = TRUE,
         fill = TRUE,
         idcol = NULL
