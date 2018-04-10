547ea4d9e10d001682394a67967224dcf53b992a
nevrome <clemens@nevrome.de>
2018-04-10 21:08:54 +0200

    major bug in relations generators: attempt to fix vertical relations

--- R/relations_generator_vertical.R
+++ R/relations_generator_vertical.R
@@ -9,23 +9,24 @@
 #' @export
 generate_vertical_relations <- function(settings) {
 
-  population <- settings@population[order(settings@population$birth_time), ]
-  
-  population_by_unit <- split(population, population$unit)
+  population_by_unit <- split(settings@population, settings@population$unit)
   
   relations_by_unit <- lapply(
     population_by_unit,
     function(population) {
       humans <- population$id
       
-      from <- rep(humans[-(1:100)], each = 2)
-      to <- floor(
+      from_index <- rep(101:length(humans), each = 2)
+      to_index <- floor(
         stats::runif(
           length(from), 
-          min = from - 100, 
-          max = from
+          min = from_index - 100, 
+          max = from_index
         )
       )
+      
+      from <- humans[from_index]
+      to <- humans[to_index]
       
       vertical_relations <- tibble::tibble(
         from = from, 
