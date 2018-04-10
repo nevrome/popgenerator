64bb8d812bce6fc8a3f665821f5ca2cf6ea15797
nevrome <clemens@nevrome.de>
2018-04-10 17:18:10 +0200

    first draft of a workflow to plot the giant population networks by reduction to units and timeblocks

---
+++ playground/plot_population_network.R
@@ -0,0 +1,67 @@
+test2$populations[[4]] -> pop
+test2$relations[[1]] -> rel
+
+pop_groups <- pop %>% 
+  dplyr::group_by(unit) %>%
+  dplyr::mutate(
+    timeblock = plyr::round_any(birth_time, 100, f = floor)
+  ) %>%
+  dplyr::group_by(unit, timeblock) %>%
+  dplyr::summarise(
+    n = n()
+  )
+
+frommer <- dplyr::left_join(
+  rel[, c("from")],
+  pop,
+  by = c("from" = "id")
+) %>%
+  dplyr::select(
+    from, birth_time, unit
+  ) %>%
+  dplyr::rename(
+    "from_birth_time" = "birth_time",
+    "from_unit" = "unit"
+  )
+
+toer <- dplyr::left_join(
+  rel[, c("to")],
+  pop,
+  by = c("to" = "id")
+) %>%
+  dplyr::select(
+    to, birth_time, unit
+  ) %>%
+  dplyr::rename(
+    "to_birth_time" = "birth_time",
+    "to_unit" = "unit"
+  )
+
+rel2 <- cbind(frommer, toer)
+
+rel3 <- rel2 %>%
+  dplyr::filter(
+    from_unit != to_unit
+  ) %>%
+  dplyr::mutate(
+    timeblock_from = plyr::round_any(from_birth_time, 100, f = floor),
+    timeblock_to = plyr::round_any(to_birth_time, 100, f = floor)
+  ) %>%
+  dplyr::select(
+    -from, -to, -from_birth_time, -to_birth_time
+  )
+
+rel4 <- rel3 %>% dplyr::group_by(
+  from_unit, to_unit, timeblock_from, timeblock_to
+) %>%
+  dplyr::summarise(
+    n = n()
+  )
+
+rel5 <- rel4 %>% dplyr::filter(
+  n > 10
+)
+
+library(ggplot2)
+ggplot(pop_groups) +
+  geom_point(aes(x = unit, y = timeblock, size = n))
