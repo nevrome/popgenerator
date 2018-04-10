1627a7b33a5e929484730f481052a7517ca4d48a
nevrome <clemens@nevrome.de>
2018-04-10 15:23:27 +0200

    moved function

--- R/general_helper_functions.R
+++ R/general_helper_functions.R
@@ -1,1 +1,19 @@
 resample <- function(x, ...) x[sample.int(length(x), ...)]
+
+#' get n attributes according to attribute distribution
+#'
+#' @param n integer amount
+#' @param distribution_function function probability distribution
+#' @param range vector value range to query
+#'
+#' @return vector of attributes
+#'
+#' @export
+get_attribute <- function(n = NA, distribution_function, range) {
+  resample(
+    range, 
+    size = n, 
+    replace = TRUE, 
+    prob = distribution_function(range)
+  )
+}
