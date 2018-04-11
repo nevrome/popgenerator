5fb04f66e3e21d76a017c05cfb703c82643578b6
nevrome <clemens@nevrome.de>
2018-04-11 09:42:54 +0200

    slept over the the horizontal relations construction -- previous attempt was seemingly much to complicated

--- R/relations_generator_horizontal.R
+++ R/relations_generator_horizontal.R
@@ -17,30 +17,14 @@ generate_horizontal_relations <- function(settings) {
   
       humans <- population$id
       
-      from_index <- rep(101:length(humans), each = settings@amount_friends)
-      
-      to_index_begin <- floor(
+      from_index <- rep(101:(length(humans) - 100), each = settings@amount_friends)
+      to_index <- floor(
         stats::runif(
-          100, 
-          min = 1, 
-          max = 1:100 + 100
+          length(from_index), 
+          min = from_index - 100, 
+          max = from_index + 100
         )
       )
-      to_index_middle <- floor(
-        stats::runif(
-          length(from_index) - 200, 
-          min = 101:(length(humans) - 100) - 100,
-          max = 101:(length(humans) - 100) + 100
-        )
-      )
-      to_index_end <- floor(
-        stats::runif(
-          100, 
-          min = (length(humans) - 100):length(humans) - 100, 
-          max = (length(humans) - 100):length(humans)
-        )
-      )
-      to_index <- c(to_index_begin, to_index_middle, to_index_end)
       
       vertical_relations <- tibble::tibble(
         from = humans[from_index], 
