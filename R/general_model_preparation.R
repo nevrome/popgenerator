3758b00c5036dde39af12d737c9e4adad2a2600b
nevrome <clemens@nevrome.de>
2018-09-10 20:00:25 +0200

    reduced permanent writing to file system to necessary minimum

--- R/general_model_preparation.R
+++ R/general_model_preparation.R
@@ -2,12 +2,13 @@
 #'
 #' @param x models_grid data.frame
 #' @param dir_path directory path where to store the output files
+#' @param only_idea_proportions should only the final result file be stored permanently
 #' @param cores how many parallel threads should be used
 #'
 #' @return modified models_grid with additional columns
 #' 
 #' @export
-run_simulation <- function(x, dir_path, cores = parallel::detectCores()) {
+run_simulation <- function(x, dir_path, only_idea_proportions = TRUE, cores = parallel::detectCores()) {
   
   x$cutting_points_for_compuation <- rep(seq(1, ceiling(nrow(x)/cores)), each = cores)[1:nrow(x)] 
   x_cut <- split(x, as.factor(x$cutting_points_for_compuation))
@@ -26,6 +27,16 @@ run_simulation <- function(x, dir_path, cores = parallel::detectCores()) {
       # prepare ideas
       ideas_settings <- init_ideas_settings(y, populations)
       
+      # only if stated explicitly the intermediate result will be stored 
+      current_dir_path <- dir_path
+      if (only_idea_proportions) {
+        path <- file.path(tempdir(), "gluesless")
+        if (!dir.exists(path)) {
+          dir.create(path)
+        }
+        current_dir_path <- path
+      }
+      
       # write prepared data to file system
       write_models_to_files(
         y$model_id,
@@ -33,14 +44,14 @@ run_simulation <- function(x, dir_path, cores = parallel::detectCores()) {
         relations,
         ideas_settings,
         y$timeframe,
-        dir_path
+        current_dir_path
       )
       
       # run simulation
       simulation_results <- run_gluesless(
         app_path = "../gluesless/build/gluesless",
-        input_file_dir = dir_path,
-        output_file_dir = dir_path,
+        input_file_dir = current_dir_path,
+        output_file_dir = current_dir_path,
         models_to_run = y$model_id
       )
       
