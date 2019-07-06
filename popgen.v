3c6ced7baddfb7a21a19686aae52132f259c3384
nevrome <clemens@nevrome.de>
2019-07-06 23:34:54 +0200

    added first draft of social coordinate attribution

--- popgen.v
+++ popgen.v
@@ -1,4 +1,5 @@
 import rand
+import math
 
 fn main() {
 
@@ -6,23 +7,28 @@ fn main() {
 	
 	// #### paramters ####
 	end_time := 100
-	generation_length := 25
+	end_social := 100
+	generation_length := 25.0
 
 	// #### logic ####
 	
 	// calculate population development
-	cumsum_of_popsize_array := cum_popsize_distribution(end_time)
+	cumsum_of_popsize_array := cum_popsize_distribution(end_social)
 	total_popsize := cumsum_of_popsize_array[end_time]
-	total_number_of_entities := total_popsize / generation_length
+	total_number_of_entities := int(math.round(total_popsize / generation_length))
 
+	// calculate social structure
+	cumsum_of_social_array := cum_social_distribution(end_time)
+	total_social := cumsum_of_social_array[end_social]
+	
 	mut entities_collector := []Entity
 	for entity_counter := 1; entity_counter <= total_number_of_entities; entity_counter++ {
 		// temporal distribution
-		entity_time := where_in_cum_array(random_integer(total_popsize), cumsum_of_popsize_array)
+		entity_time := where_in_cum_array(f64(random_integer(int(total_popsize))), cumsum_of_popsize_array)
 		// social distribution
-		//TODO
+		entity_social := where_in_cum_array(f64(random_integer(int(total_social))), cumsum_of_social_array)
 		// construct entities		
-		entities_collector << Entity{time: entity_time, social: 000}
+		entities_collector << Entity{time: entity_time, social: entity_social}
 	}
 
 	for entity in entities_collector {
@@ -47,13 +53,13 @@ fn (e Entity) print() {
 } 
 
 // #### population size along temporal space ####
-fn popsize_distribution(time int) int {
-  return 1 * time + 20
+fn popsize_distribution(time int) f64 {
+  return 1.25 * time + 20
 }
 
-fn cum_popsize_distribution(end_time int) []int {
+fn cum_popsize_distribution(end_time int) []f64 {
 	mut time := 1
-	mut cum_sum := []int
+	mut cum_sum := []f64
 	cum_sum << 0
 	for time <= end_time {
 		cum_sum << cum_sum[time - 1] + popsize_distribution(time)
@@ -63,15 +69,27 @@ fn cum_popsize_distribution(end_time int) []int {
 }
 
 // #### social structure along the social space ####
+fn social_distribution(social int) f64 {
+  return 0.5 * math.cos(0.314 * social + 3.14) + 0.5
+}
 
-// TODO
+fn cum_social_distribution(end_social int) []f64 {
+	mut social := 1
+	mut cum_sum := []f64
+	cum_sum << 0.0
+	for social <= end_social {
+		cum_sum << cum_sum[social - 1] + social_distribution(social)
+		social++
+	}
+	return(cum_sum)
+}
 
 // #### helper functions ####
 fn random_integer(max int) int {
 	return rand.next(max)
 }
 
-fn where_in_cum_array(x int, numbers []int) int {
+fn where_in_cum_array(x f64, numbers []f64) int {
 	mut i := 0
 	for i < numbers.len {
 		num_1 := numbers[i]
