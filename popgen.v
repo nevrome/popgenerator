46490c1e28ce36f6261056f5911b1941d92311dd
nevrome <clemens@nevrome.de>
2019-07-06 18:33:43 +0200

    further experiments with v

--- popgen.v
+++ popgen.v
@@ -9,6 +9,8 @@ fn main() {
 	
 	huhu.print()
 	
+	println(cum_popsize_distribution(3, 10).str())
+
 }
 
 struct Entity {
@@ -22,12 +24,29 @@ fn (e Entity) print() {
 
 fn entity_generator(start_time int, end_time int) Entity {
 	
-	//rand.seed(1234)
-	//rand.next(10)
 	new_entity := Entity{time: 10, social: 15} 
 
 	return new_entity
 
+}
+
+fn popsize_distribution(time int) int {
+  return 2 * time + 20
+}
+
+fn cum_popsize_distribution(start_time int, end_time int) int {
+	mut time := start_time	
+	mut cum_sum := 0
+	for time <= end_time {
+		cum_sum += popsize_distribution(time)
+		time++
+	}
+	return(cum_sum)
+}
+
+fn random_integer(max int) int {
+	rand.seed()
+	return rand.next(max)
 }
 
 
