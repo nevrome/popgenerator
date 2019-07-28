a34c098b3ca628b1f6d406a4889e777194afb592
Clemens Schmid <clemens@nevrome.de>
2019-07-28 18:34:55 +0200

    much better time-social coordinate generator

--- popgen.v
+++ popgen.v
@@ -4,28 +4,40 @@ import os
 
 fn main() {
 
-	rand.seed(123)
+	rand.seed(1463233)
 
-	// #### paramters ####
-	end_time := 100
-	end_social := 100
-	generation_length := 25.0
+	// #### parameters ####
+	end_time := 1000
+	end_social := 1000
+	number_of_entities := 1000
+
+	// #### create sequences####
+	mut time_arr := [0; end_time]
+	for t := 0; t < end_time; t++ {
+		time_arr[t] = t
+	}
+	mut social_arr := [0; end_social]
+	for s := 0; s < end_social; s++ {
+		social_arr[s] = s
+	}
+
+	// #### calculate frequencies ####
+	mut time_freq := [0; end_time]
+	for t := 0; t < end_time; t++ {
+		time_freq[t] = temporal_distribution(t)
+	}
+	mut social_freq := [0; end_social]
+	for s := 0; s < end_social; s++ {
+		social_freq[s] = social_distribution(s)
+	}
 
 	// #### logic ####
-
-	// calculate population development
-	total_popsize := integral_popsize_distribution(end_time)
-	total_number_of_entities := int(math.round(total_popsize / generation_length))
-
-	// calculate social structure
-	total_social := integral_social_distribution(end_social)
-
 	mut entities_collector := []Entity
-	for entity_counter := 1; entity_counter <= total_number_of_entities; entity_counter++ {
-		// temporal distribution
-		entity_time := inverse_integral_popsize_distribution(f64(random_integer(int(total_popsize))))
-		// social distribution
-		entity_social := inverse_integral_social_distribution(f64(random_integer(int(total_social))))
+	for entity_counter := 1; entity_counter <= number_of_entities; entity_counter++ {
+		// draw temporal coordinate
+		entity_time := random_integer_distribution(time_arr, time_freq, end_time)
+		// draw social coordinate
+		entity_social := random_integer_distribution(social_arr, social_freq, end_social)
 		// construct entities
 		entities_collector << Entity{time: entity_time, social: entity_social}
 	}
@@ -50,8 +62,8 @@ struct SocialWorld {
 
 // #### Entity ####
 struct Entity {
-	time f64
-	social f64
+	time int
+	social int
 }
 
 fn (e Entity) print() string {
@@ -59,46 +71,51 @@ fn (e Entity) print() string {
 }
 
 // #### population size along temporal space ####
-fn popsize_distribution(x int) f64 {
-  return 2 * x
-}
-
-fn integral_popsize_distribution(x int) f64 {
-	return 2 * x * x
-}
-
-fn inverse_integral_popsize_distribution(y f64) f64 {
-	return math.sqrt(y / 2)
+fn temporal_distribution(t int) int {
+	x := f64(t)
+	res := 20.0 + 0.5 * x
+  return int(math.round(res))
 }
 
 // #### social structure along the social space ####
-fn social_distribution(x int) f64 {
-  return 2 * x
+fn social_distribution(s int) int {
+	x := f64(s)
+	res := 5.0 * math.sin(0.01 * (x - 0.1)) + 5.0
+  return int(math.round(res))
 }
 
-fn integral_social_distribution(x int) f64 {
-	return 2 * x * x
-}
-
-fn inverse_integral_social_distribution(y f64) f64 {
-	return math.sqrt(y / 2)
-}
-
-// #### helper functions ####
+// #### random number generation ####
 fn random_integer(max int) int {
 	return rand.next(max)
 }
 
-fn where_in_cum_array(x f64, numbers []f64) int {
-	mut i := 0
-	for i < numbers.len {
-		num_1 := numbers[i]
-		num_2 := numbers[i + 1]
-		if x >= num_1 && x <= num_2 {
-			//println(num_1.str() + ' > ' + x.str() + ' < ' + num_2.str() + ' = ' + i.str())
-			break
+// https://www.geeksforgeeks.org/random-number-generator-in-arbitrary-probability-distribution-fashion/
+// returns a random number from arr[] according to
+// distribution array defined by freq[]. n is size of arrays.
+fn random_integer_distribution(arr[] int, freq[] int, n int) int {
+		// Create and fill prefix array
+    mut prefix := [0; n]
+    prefix[0] = freq[0]
+		for i := 1; i < n; i++ {
+      prefix[i] = prefix[i - 1] + freq[i]
 		}
-		i++
-	}
-	return i
+    // prefix[n-1] is sum of all frequencies. Generate a random number
+    // with value from 1 to this sum
+    r := (random_integer(prefix[n - 1])) + 1
+    // Find index of ceiling of r in prefix arrat
+  	indexc := find_ceil(prefix, r, 0, n - 1)
+    return arr[indexc]
+}
+
+// Utility function to find ceiling of r in arr[l..h]
+fn find_ceil(arr[] int, r int, l int, h int) int {
+		mut mid := 0
+		for {
+			mid = (l + h) / 2
+			if r > arr[mid] { l = mid + 1 }  else { h = mid }
+			if !(l < h) { break }
+		}
+		mut res := 0
+		if arr[l] >= r { res = l } else { res = -1 }
+    return res
 }
