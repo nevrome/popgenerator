257b4b950161f03c1d35b6159d6f0e5b4ff69533
Clemens Schmid <clemens@nevrome.de>
2019-08-12 22:41:40 +0200

    small changes in distribution functions

--- distribution_functions.v
+++ distribution_functions.v
@@ -8,13 +8,13 @@ fn temporal_distribution(i int) int {
 // #### x structure along the social space ####
 fn x_distribution(i int) int {
 	x := f64(i)
-	res := 5.0 * math.sin(0.01 * (x - 0.1)) + 5.0
+	res := 10.0 * math.sin(0.01 * (x - 0.1)) + 20.0
   return int(math.round(res))
 }
 
 // #### y structure along the social space ####
 fn y_distribution(i int) int {
 	x := f64(i)
-	res := 5.0 * math.sin(0.01 * (x - 0.1)) + 5.0
+	res := 10.0 * math.sin(0.01 * (x - 0.1)) + 20.0
   return int(math.round(res))
 }
